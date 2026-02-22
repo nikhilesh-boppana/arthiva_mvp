f96b38c64e5b4b4dddd25ed83181918fc4fd4fb3
Nikhilesh <boppananikhilesh@gmail.com>
2026-02-21 21:20:03 -0600

    Create AarthivaApplication.java

---
+++ backend/src/main/java/com/arthiva/AarthivaApplication.java
@@ -0,0 +1,11 @@
+package com.arthiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarthivaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AarthivaApplication.class, args);
+    }
+}
